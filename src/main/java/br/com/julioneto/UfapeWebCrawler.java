b62ba4ce790690eecf06d138b40e5c3042a8eafe
Julio Neto <nettojulio@hotmail.com>
2025-08-13 16:37:09 -0300

    Initial commit

---
+++ src/main/java/br/com/julioneto/UfapeWebCrawler.java
@@ -0,0 +1,7 @@
+package br.com.julioneto;
+
+public class UfapeWebCrawler {
+    public static void main(String[] args) {
+        System.out.println("Starting Ufape Web Crawler...");
+    }
+}
---
+++ src/test/java/br/com/julioneto/UfapeWebCrawlerTest.java
@@ -0,0 +1,11 @@
+package br.com.julioneto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UfapeWebCrawlerTest {
+    @Test
+    void exampleTest() {
+        Assertions.assertEquals(2, 1+1);
+    }
+}
