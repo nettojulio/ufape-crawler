90bccb1cdc8155b29daf7f3164058d024ef61462
pedro tobias <pedrotsg123@gmail.com>
2025-08-15 16:27:14 -0300

    feat: criação de metodo para melhoria de logica de verificação de links já visitados

--- src/main/java/br/com/julioneto/UfapeWebCrawler.java
+++ src/main/java/br/com/julioneto/UfapeWebCrawler.java
@@ -4,6 +4,8 @@ import br.com.julioneto.models.Grafo;
 import br.com.julioneto.models.Link;
 import br.com.julioneto.service.WebScraperService;
 
+import java.util.List;
+
 public class UfapeWebCrawler {
     public static void main(String[] args) {
         Grafo grafo = new Grafo();
@@ -15,6 +17,18 @@ public class UfapeWebCrawler {
         System.out.println("\n--- Relatório Final ---");
         for (Link link : grafo.getLinks().values()) {
             System.out.println("URL: " + link.getUrl() + ", Status: " + link.getStatusCode());
+            System.out.println("Total de paginas visitadas: " + grafo.getLinks().size());
+
+            List<Link> linksQuebrados = grafo.getLinksQuebrados();
+
+            if (!linksQuebrados.isEmpty()) {
+                System.out.println("\n--- Links Quebrados (" + linksQuebrados.size() + ") ---");
+                for (Link links : linksQuebrados) {
+                    System.out.println("URL: " + links.getUrl() + ", Status: " + links.getStatusCode());
+                }
+            } else {
+                System.out.println("\nNenhum link quebrado encontrado.");
+            }
         }
     }
 }
