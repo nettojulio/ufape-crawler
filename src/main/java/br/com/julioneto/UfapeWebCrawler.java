96f445e60aa833698fd27bf34c1956e7007e57b3
NETOXSreserva <cachorrocomunista60223@gmail.com>
2025-08-17 20:08:27 -0300

    agora o codigo ta se comunicando com a API, nao com o jsoup

--- src/main/java/br/com/julioneto/UfapeWebCrawler.java
+++ src/main/java/br/com/julioneto/UfapeWebCrawler.java
@@ -3,34 +3,30 @@ package br.com.julioneto;
 import br.com.julioneto.models.Grafo;
 import br.com.julioneto.models.Link;
 import br.com.julioneto.service.JsonService;
-import br.com.julioneto.service.WebScraperService;
+import br.com.julioneto.service.BuscaEmLarguraService;
 
 import java.util.List;
 
 public class UfapeWebCrawler {
     public static void main(String[] args) {
         Grafo grafo = new Grafo();
-        WebScraperService scraper = new WebScraperService(grafo);
+        BuscaEmLarguraService crawler = new BuscaEmLarguraService(grafo);
 
         String urlInicial = "https://ufape.edu.br/";
-        scraper.iniciarCrawl(urlInicial);
+        crawler.iniciarCrawl(urlInicial);
 
         System.out.println("\n--- Relatório Final ---");
-        for (Link link : grafo.getLinks().values()) {
-            System.out.println("URL: " + link.getUrl() + ", Status: " + link.getStatusCode());
-        }
-        System.out.println("Total de paginas visitadas: " + grafo.getLinks().size());
+        System.out.println("Total de páginas processadas: " + grafo.getLinks().size());
 
         List<Link> linksQuebrados = grafo.getLinksQuebrados();
         if (!linksQuebrados.isEmpty()) {
             System.out.println("\n--- Links Quebrados (" + linksQuebrados.size() + ") ---");
-            for (Link links : linksQuebrados) {
-                System.out.println("URL: " + links.getUrl() + ", Status: " + links.getStatusCode());
+            for (Link link : linksQuebrados) {
+                System.out.println("URL: " + link.getUrl() + ", Status: " + link.getStatusCode());
             }
         } else {
             System.out.println("\nNenhum link quebrado encontrado.");
         }
-
 
         JsonService jsonService = new JsonService();
         jsonService.salvarGrafoComoJson(grafo, "grafo_salvo.json");
